b22449c2fb6ace890ad2eafcff308dba630e5e93
kbetlach <kbetlach@gmail.com>
2021-04-27 12:30:43 -0500

    created basic file structure. created basic start page and imported default react bootstrap table component

---
+++ mhr_bestiary/src/components/Pages/Start.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./pages.css";
+
+function Start() {
+    return (
+        <div>
+            <div className="container w3-container w3-center w3-animate-opacity">
+                <div className="jumbotron">
+                    <div className="h1">
+                        Monster Hunter Rise Bestiary
+                    </div>
+                    <div className="h2">
+                        Your one-stop-shop for all monster information.
+                    </div>
+                    <div className="button">
+                        Begin
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Start;
