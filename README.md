b472bfb70e60d39917ba183ec0a593a562fa207c
kbetlach <kbetlach@gmail.com>
2021-05-06 11:26:43 -0500

    created working nav tabs for monster page. completed low rank izuchi table

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mhr_bestiary
+
+Tab Tutorial: https://github.com/Ziratsu/YT-REACT-TABS
+Dynamic Table Tutorial: https://www.youtube.com/watch?v=nV7Mf77GiOc&t=356s
--- mhr_bestiary/src/components/Pages/LargeMonsterPages/GreatIzuchi.js
+++ mhr_bestiary/src/components/Pages/LargeMonsterPages/GreatIzuchi.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import Table from 'react-bootstrap/Table'
+import {useState} from "react";
+import Table from 'react-bootstrap/Table';
 
 function GreatIzuchi() {
 
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
     const lowRankMaterials = [
-        {material: "Great Izuchi Hide", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
-        {material: "Great Izuchi Pelt", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
-        {material: "Great Izuchi Tail", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
-        {material: "Monster Bone S", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
-        {material: "Screamer Sac", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
-        {material: "Wyvern Tear", target: "21%", capture: "26%", break: "20% (Tail)", carves: "43% (Body)", dropped: "30%"},
+        {
+            material: "Great Izuchi Hide",
+            target: "21%",
+            capture: "26%",
+            break: "20% (Tail)",
+            carves: "43% (Body)",
+            dropped: "30%, 60%"
+        },
+        {
+            material: "Great Izuchi Pelt",
+            target: "36%",
+            capture: "39%",
+            break: "30% (Head)",
+            carves: "33% (Body)",
+            dropped: "20%, 40"
+        },
+        {
+            material: "Great Izuchi Tail",
+            target: "5%",
+            capture: "15%",
+            break: "80% (Tail)",
+            carves: "-",
+            dropped: "-"
+        },
+        {
+            material: "Screamer Sac",
+            target: "26%",
+            capture: "20%",
+            break: "70% (Head)",
+            carves: "24% (Body)",
+            dropped: "-"
+        }, {
+            material: "Monster Bone S",
+            target: "12%",
+            capture: "-",
+            break: "-",
+            carves: "-",
+            dropped: "-"
+        }, {
+            material: "Wyvern Tear",
+            target: "-",
+            capture: "-",
+            break: "-",
+            carves: "-",
+            dropped: "50%"
+        },
     ]
 
     const renderLowRankMaterials = (low, index) => {
         return (
             <tr key={index}>
-                <td>{low.material}</td>
-                <td>{low.target}</td>
-                <td>{low.capture}</td>
-                <td>{low.break}</td>
-                <td>{low.carves}</td>
-                <td>{low.dropped}</td>
+                <td>{
+                    low.material
+                }</td>
+                <td>{
+                    low.target
+                }</td>
+                <td>{
+                    low.capture
+                }</td>
+                <td>{
+                    low.break
+                }</td>
+                <td>{
+                    low.carves
+                }</td>
+                <td>{
+                    low.dropped
+                }</td>
             </tr>
         )
     }
@@ -29,24 +87,57 @@ function GreatIzuchi() {
         <div>
             <div className="container w3-container w3-center w3-animate-opacity">
                 <div className="jumbotron">
-                    <Table responsive>
 
-                        <thead>
-                            <tr>
-                                <th>Material</th>
-                                <th>Target Reward</th>
-                                <th>Capture Rewards</th>
-                                <th>Break Part Rewards</th>
-                                <th>Carves</th>
-                                <th>Dropped Materials</th>
-                            </tr>
-                        </thead>
+                    <div className="bloc-tabs">
+                        <button className={
+                                toggleState === 1 ? "tabs active-tabs" : "tabs"
+                            }
+                            onClick={
+                                () => toggleTab(1)
+                        }>
+                            Low Rank Materials
+                        </button>
+                        <button className={
+                                toggleState === 2 ? "tabs active-tabs" : "tabs"
+                            }
+                            onClick={
+                                () => toggleTab(2)
+                        }>
+                            High Rank Materials
+                        </button>
+                    </div>
 
-                        <tbody>
-                            {lowRankMaterials.map(renderLowRankMaterials)}
-                        </tbody>
-                        
-                    </Table>
+                    <div className="content-tabs">
+                        <div className={
+                            toggleState === 1 ? "content  active-content" : "content"
+                        }>
+                            
+                            <Table responsive>
+
+                                <thead>
+                                    <tr>
+                                        <th>Material</th>
+                                        <th>Target Reward</th>
+                                        <th>Capture Rewards</th>
+                                        <th>Break Part Rewards</th>
+                                        <th>Carves</th>
+                                        <th>Dropped Materials</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody> {
+                                    lowRankMaterials.map(renderLowRankMaterials)
+                                } </tbody>
+
+                            </Table>
+                        </div>
+
+                        <div className={
+                            toggleState === 2 ? "content  active-content" : "content"
+                        }>
+                            <h2>Content 2</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
