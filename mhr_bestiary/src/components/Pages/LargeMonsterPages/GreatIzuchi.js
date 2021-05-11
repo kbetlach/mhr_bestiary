0afedbb0008e407eb40154ffe4b7ef344572a9b0
kbetlach <kbetlach@gmail.com>
2021-05-11 12:20:11 -0500

    added high rank button to low rank state and filled in table. can toggle between high and low

--- mhr_bestiary/src/components/Pages/LargeMonsterPages/GreatIzuchi.js
+++ mhr_bestiary/src/components/Pages/LargeMonsterPages/GreatIzuchi.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import Table from 'react-bootstrap/Table';
+import Button from '../../Button/Button';
 
 function GreatIzuchi() {
 
@@ -24,7 +25,7 @@ function GreatIzuchi() {
             capture: "39%",
             break: "30% (Head)",
             carves: "33% (Body)",
-            dropped: "20%, 40"
+            dropped: "20%, 40%"
         },
         {
             material: "Great Izuchi Tail",
@@ -58,27 +59,91 @@ function GreatIzuchi() {
         },
     ]
 
+    const highRankMaterials = [
+        {
+            material: "Great Izuchi Hide +",
+            target: "23%",
+            capture: "27%",
+            break: "20% (Tail)",
+            carves: "44% (Body)",
+            dropped: "29%, 60%"
+        },
+        {
+            material: "Great Izuchi Pelt +",
+            target: "39%",
+            capture: "39%",
+            break: "32% (Head)",
+            carves: "35% (Body)",
+            dropped: "20%, 40%"
+        },
+        {
+            material: "Great Izuchi Tail +",
+            target: "5%",
+            capture: "15%",
+            break: "80% (Tail)",
+            carves: "-",
+            dropped: "-"
+        },
+        {
+            material: "Screamer Sac",
+            target: "18%",
+            capture: "16%",
+            break: "65% (Head)",
+            carves: "20% (Body)",
+            dropped: "-"
+        }, {
+            material: "Monster Bone +",
+            target: "13%",
+            capture: "-",
+            break: "-",
+            carves: "-",
+            dropped: "-"
+        }, {
+            material: "Bird Wyvern Gem",
+            target: "2%",
+            capture: "3%",
+            break: "3% (Head)",
+            carves: "1% (Body)",
+            dropped: "1%"
+        }, {
+            material: "Wyvern Tear",
+            target: "-",
+            capture: "-",
+            break: "-",
+            carves: "-",
+            dropped: "30%"
+        }, {
+            material: "Large Wyvern Tear",
+            target: "-",
+            capture: "-",
+            break: "-",
+            carves: "-",
+            dropped: "20%"
+        }
+    ]
+
     const renderLowRankMaterials = (low, index) => {
         return (
             <tr key={index}>
-                <td>{
-                    low.material
-                }</td>
-                <td>{
-                    low.target
-                }</td>
-                <td>{
-                    low.capture
-                }</td>
-                <td>{
-                    low.break
-                }</td>
-                <td>{
-                    low.carves
-                }</td>
-                <td>{
-                    low.dropped
-                }</td>
+                <td>{low.material}</td>
+                <td>{low.target}</td>
+                <td>{low.capture}</td>
+                <td>{low.break}</td>
+                <td>{low.carves}</td>
+                <td>{low.dropped}</td>
+            </tr>
+        )
+    }
+
+    const renderHighRankMaterials = (high, index) => {
+        return (
+            <tr key={index}>
+                <td>{high.material}</td>
+                <td>{high.target}</td>
+                <td>{high.capture}</td>
+                <td>{high.break}</td>
+                <td>{high.carves}</td>
+                <td>{high.dropped}</td>
             </tr>
         )
     }
@@ -86,55 +151,33 @@ function GreatIzuchi() {
     return (
         <div>
             <div className="container w3-container w3-center w3-animate-opacity">
+                <h1>Great Izuchi</h1>
                 <div className="jumbotron">
 
                     <div className="bloc-tabs">
-                        <button className={
-                                toggleState === 1 ? "tabs active-tabs" : "tabs"
-                            }
-                            onClick={
-                                () => toggleTab(1)
-                        }>
-                            Low Rank Materials
+                        <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>
+                            Materials
                         </button>
-                        <button className={
-                                toggleState === 2 ? "tabs active-tabs" : "tabs"
-                            }
-                            onClick={
-                                () => toggleTab(2)
-                        }>
-                            High Rank Materials
-                        </button>
-                        <button className={
-                                toggleState === 3 ? "tabs active-tabs" : "tabs"
-                            }
-                            onClick={
-                                () => toggleTab(3)
-                        }>
+
+                        <button className={toggleState === 3 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(3)}>
                             Ecology
                         </button>
-                        <button className={
-                                toggleState === 4 ? "tabs active-tabs" : "tabs"
-                            }
-                            onClick={
-                                () => toggleTab(4)
-                        }>
+
+                        <button className={toggleState === 4 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(4)}>
                             Physiology
                         </button>
-                        <button className={
-                                toggleState === 5 ? "tabs active-tabs" : "tabs"
-                            }
-                            onClick={
-                                () => toggleTab(5)
-                        }>
+
+                        <button className={toggleState === 5 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(5)}>
                             Ailments
                         </button>
                     </div>
 
                     <div className="content-tabs">
-                        <div className={
-                            toggleState === 1 ? "content  active-content" : "content"
-                        }>
+                        <div className={toggleState === 1 ? "content  active-content" : "content"}>
+
+                        <Button onClick={() => toggleTab(2)}>
+                            High Rank
+                        </Button>
                             
                             <Table responsive>
 
@@ -156,11 +199,32 @@ function GreatIzuchi() {
                             </Table>
                         </div>
 
-                        <div className={
-                            toggleState === 2 ? "content  active-content" : "content"
-                        }>
-                            <h2>Content 2</h2>
+                        <div className={toggleState === 2 ? "content  active-content" : "content"}>
+
+                            <Button onClick={() => toggleTab(1)}>
+                                Low Rank
+                            </Button>
+
+                            <Table responsive>
+                                <thead>
+                                    <tr>
+                                        <th>Material</th>
+                                        <th>Target Reward</th>
+                                        <th>Capture Rewards</th>
+                                        <th>Break Part Rewards</th>
+                                        <th>Carves</th>
+                                        <th>Dropped Materials</th>
+                                    </tr>
+                                </thead>
+
+                            <tbody> {
+                                highRankMaterials.map(renderHighRankMaterials)} 
+                            </tbody>
+
+                        </Table>
+
                         </div>
+        
                         <div className={
                             toggleState === 3 ? "content  active-content" : "content"
                         }>
